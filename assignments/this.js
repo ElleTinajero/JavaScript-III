ff49e0ae3dde44a3d036ec4ce685c31604288c26
Michelle Tinajero <elletinajero11@gmail.com>
2019-09-12 17:55:38 -0500

    added to this

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding gives you access to the window scope or global scope. (self explanatory)
+* 2. Implicit Binding gives you access to the object after this. 
+* 3. New Binding gives you access to a constructor function's object.
+* 4. Explicit Binding works with inheritance and gives you access when you use .call .apply or super?
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +12,13 @@
 // Principle 1
 
 // code example for Window Binding
+
+const myFunction = function number(11) {
+    console.log(this);
+    return 11
+}
+
+number(11);
 
 // Principle 2
 
